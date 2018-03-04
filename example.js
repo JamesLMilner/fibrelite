8dc9b769fcaddca62de5bdb958e53a714d76b674
James Milner <jamesmilner22@hotmail.com>
2018-03-04 19:56:58 +0000

    Initial commit

---
+++ example.js
@@ -0,0 +1,135 @@
+const dataProcessingAsync = async (input) => {
+
+    // returns the byte length of an utf8 inputing
+    const s = input.length;
+    for (let i=input.length-1; i>=0; i--) {
+        const code = input.charCodeAt(i);
+        if (code > 0x7f && code <= 0x7ff) s++;
+        else if (code > 0x7ff && code <= 0xffff) s+=2;
+        if (code >= 0xDC00 && code <= 0xDFFF) i--; //trail surrogate
+    }
+
+    // Synthetic wait
+    const ms = 333;
+    const start = Date.now();
+    let now = start;
+    while (now - start < ms) {
+        now = Date.now();
+    }
+
+    return s;
+};
+
+
+const syncExample = () => {
+    const syncInput = document.getElementById("syncUserInput");
+    const syncMessage = document.getElementById("syncUserMessage");
+    let syncLatest;
+    let syncBytes = 0;
+
+    syncInput.addEventListener("keyup", async (event) => {
+        
+        const timestamp = new Date();
+        syncLatest = timestamp;
+
+        syncBytes = await dataProcessingAsync(event.target.value);
+        
+        requestAnimationFrame(() => {
+            if (timestamp.getTime() === syncLatest.getTime()) {
+                syncMessage.innerHTML = syncBytes + " bytes in user input"
+            }
+        });
+
+    });
+}
+
+syncExample();
+
+const rICExample = () => {
+    const rICInput = document.getElementById("rICUserInput");
+    const rICMessage = document.getElementById("rICUserMessage");
+    let rICLatest;
+    let rICBytes = 0;
+
+    rICInput.addEventListener("keyup", async (event) => {
+        
+        const timestamp = new Date();
+        rICLatest = timestamp;
+        
+        requestIdleCallback(async () => {
+            rICBytes = await dataProcessingAsync(event.target.value);
+        })
+        
+        requestAnimationFrame(() => {
+            if (timestamp.getTime() === rICLatest.getTime()) {
+                rICMessage.innerHTML = rICBytes + " bytes in user input"
+            }
+        });
+
+    });
+}
+
+rICExample();
+
+const fibreliteExample = () => {
+
+    const fibril = new fibrelite(dataProcessingAsync);
+    const getStringBytes = fibril.prioritiseExecute; // fibril.prioritiseExecute;
+    const input = document.getElementById("userInput");
+    const message = document.getElementById("userMessage");
+    let latest;
+    let bytes = 0;
+
+    input.addEventListener("keyup", async (event) => {
+        
+        
+        const timestamp = new Date();
+        latest = timestamp;
+        bytes = await getStringBytes(event.target.value);
+        
+        requestAnimationFrame(() => {
+            if (timestamp.getTime() === latest.getTime()) {
+                message.innerHTML = bytes + " bytes in user input"
+            }
+        });
+
+    });
+
+}
+
+fibreliteExample();
+
+const setupStats = () => {
+
+    const stats = new Stats();
+    console.log(stats);
+    stats.showPanel( 0 ); // 0: fps, 1: ms, 2: mb, 3+: custom
+    stats.domElement.style.position = "fixed";
+    stats.domElement.style.top = "0";
+    stats.domElement.style.right = "0";
+    stats.domElement.style.width = 80 * 2;
+    stats.domElement.style.height = 48 * 2;
+    for (var i = 0; i <  stats.domElement.children.length; i++) {
+        stats.domElement.children[i].style.width = 80 * 2;
+        stats.domElement.children[i].style.height = 48 * 2;
+    }
+    stats.domElement.children.fo
+    document.body.appendChild( stats.domElement );
+
+    const animate = () => {
+
+        stats.begin();
+
+        // monitored code goes here
+
+        stats.end();
+
+        requestAnimationFrame( animate );
+
+    }
+
+    requestAnimationFrame( animate );
+
+};
+ 
+setupStats();
---
+++ fibrelite.js
@@ -0,0 +1,102 @@
+export default function fibrelite(asyncFunction) {
+
+    let currentThread;
+
+    /** Move an async function into its own thread.
+     *  @param {Function} asyncFunction  An (async) function to run in a Worker.
+     *  @public
+     */
+    const greenlet = (asyncFunction) => {
+
+        if (!this.cachedFn) {
+            this.cachedFn = 
+            // Register our wrapper function as the message handler
+            'onmessage=(' + (
+                // userFunc() is the user-supplied async function
+                userFunc => e => {
+                    // Invoking within then() captures exceptions in userFunc() as rejections
+                    Promise.resolve(e.data[1]).then(
+                        userFunc.apply.bind(userFunc, userFunc)
+                    ).then(
+                        // success handler - callback(id, SUCCESS(0), result)
+                        d => { postMessage([e.data[0], 0, d]); },
+                        // error handler - callback(id, ERROR(1), error)
+                        e => { postMessage([e.data[0], 1, ''+e]); }
+                    );
+                }
+            ) + ')(' + asyncFunction + ')'  // pass user-supplied function to the closure
+        }
+
+        // Create an "inline" worker (1:1 at definition time)
+        let worker = new Worker(
+                // The URL is a pointer to a stringified function (as a blob object)
+                URL.createObjectURL(
+                    new Blob([this.cachedFn])
+                )
+            ),
+
+            // A simple counter is used to generate worker-global unique ID's for RPC:
+            currentId = 0,
+
+            // Outward-facing promises store their "controllers" (`[request, reject]`) here:
+            promises = {};
+
+        /** Handle RPC results/errors coming back out of the worker.
+         *  Messages coming from the worker take the form `[id, status, result]`:
+         *    id     - counter-based unique ID for the RPC call
+         *    status - 0 for success, 1 for failure
+         *    result - the result or error, depending on `status`
+         */
+        worker.onmessage = e => {
+            // invoke the promise's resolve() or reject() depending on whether there was an error.
+            promises[e.data[0]][e.data[1]](e.data[2]);
+
+            // ... then delete the promise controller
+            promises[e.data[0]] = null;
+        };
+
+        // Return a proxy function that forwards calls to the worker & returns a promise for the result.
+        return {
+            worker: worker,
+            fn : function(args) {
+                args = [].slice.call(arguments);
+                return new Promise(function() {
+                    // Add the promise controller to the registry
+                    promises[++currentId] = arguments;
+
+                    // Send an RPC call to the worker - call(id, params)
+                    worker.postMessage([currentId, args]);
+                });
+            }
+        };
+
+    }
+
+    this.prioritiseExecute = async (value) => {
+
+        if (currentThread === undefined) {
+            currentThread = greenlet(asyncFunction)
+            return currentThread.fn(value);
+        }
+        new Promise((resolve) => {
+            resolve(currentThread.worker.terminate()); 
+        });
+        currentThread = greenlet(asyncFunction);
+        return currentThread.fn(value);
+        
+    }
+
+    this.execute = async (value) => {
+        if (currentThread === undefined) {
+            currentThread = greenlet(asyncFunction)
+            return currentThread.fn(value);
+        }
+        return currentThread.fn(value);
+    }
+
+    this.getCurrentWorker = () => {
+        return currentThread.worker;
+    }
+
+}
+
